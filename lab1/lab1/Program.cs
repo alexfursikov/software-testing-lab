0e64a8e1dc5de9f417f9995c7505e7fc0da23fbc
alexfursikov <alex.fursikov@gmail.com>
2018-09-15 00:29:05 +0300

    change objectName

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -10,19 +10,19 @@ namespace lab1
     {
         static void Main(string[] args)
         {
-            Student s1 = new Student();
-            Student s2 = new Student(new int[] { 1, 2, 3,5,2,4,6,4,3 });
-            Student s3 = new Student(new int[] { 10,7,9,8,9,10,9,7,8,8 });
-            s1.calcAverageForStudent();
-            s2.calcAverageForStudent();
-            s3.calcAverageForStudent();
+            Student student_1 = new Student();
+            Student student_2 = new Student(new int[] { 1, 2, 3,5,2,4,6,4,3 });
+            Student student_3 = new Student(new int[] { 10,7,9,8,9,10,9,7,8,8 });
+            student_1.calcAverageForStudent();
+            student_2.calcAverageForStudent();
+            student_3.calcAverageForStudent();
 
             Group group2 = new Group(2);
-            group2.addStudent(s1);
-            group2.addStudent(s2);
+            group2.addStudent(student_1);
+            group2.addStudent(student_2);
 
             Group group2 = new Group(1);
-            group2.addStudent(s3);
+            group2.addStudent(student_3);
 
             group2.calcAverageForGroup(1);
             group2.calcAverageForGroup(2);
