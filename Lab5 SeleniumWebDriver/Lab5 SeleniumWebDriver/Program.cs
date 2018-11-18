007cb66ca5c9a8dff75482cc72e756c9546b2bc0
alexfursikov <alex.fursikov@gmail.com>
2018-11-18 23:47:26 +0300

    add lab with SeleniumWebDriver

---
+++ Lab5 SeleniumWebDriver/Lab5 SeleniumWebDriver/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab5_SeleniumWebDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+            test.SearchWithValidData();
+        }
+    }
+}
---
+++ Lab5 SeleniumWebDriver/Lab5 SeleniumWebDriver/Test.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Threading;
+
+namespace Lab5_SeleniumWebDriver
+{
+    class Test
+    {
+        IWebDriver driver;
+
+        public void SearchWithValidData()
+        {
+            try
+            {
+                driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://www.s7-airlines.com/ru/");
+                IWebElement destinationInput = driver.FindElement(By.Id("flights_destination2"));
+                destinationInput.SendKeys("TestString" + Keys.Enter);
+                Thread.Sleep(1000);
+                IWebElement searchForm = driver.FindElement(By.Id("search-btn-expand-bot "));
+                searchForm.Submit();
+                Thread.Sleep(5000);
+
+            }
+            catch (Exception e)
+            {
+                driver.Close();
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
